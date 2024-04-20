16f5ae14f31bead1546ef1762c817a7c97d4f92f
jaesung-cs <jaesung.cs@gmail.com>
2024-04-20 21:41:27 +0900

    rank

--- src/pygs/engine/vulkan/shader/rank.h
+++ src/pygs/engine/vulkan/shader/rank.h
@@ -8,7 +8,7 @@ namespace shader {
 const char* rank_comp = R"shader(
 #version 460
 
-layout (local_size_x = 256, local_size_y = 1, local_size_z = 1) in;
+layout (local_size_x = 256) in;
 
 layout (set = 0, binding = 0) uniform Camera {
   mat4 projection;
