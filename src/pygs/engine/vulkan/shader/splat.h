0972fbd0a6aa4e9459c5a652cc49979a247a55f8
jaesung-cs <jaesung.cs@gmail.com>
2024-05-06 15:16:43 +0900

    splat scale rot

--- src/pygs/engine/vulkan/shader/splat.h
+++ src/pygs/engine/vulkan/shader/splat.h
@@ -19,20 +19,19 @@ void main() {
   // index [0,1,2,2,1,3], 4 vertices for a splat.
   int index = gl_VertexIndex / 4;
   vec3 ndc_position = vec3(instances[index * 10 + 0], instances[index * 10 + 1], instances[index * 10 + 2]);
-  vec3 scale_rot = vec3(instances[index * 10 + 3], instances[index * 10 + 4], instances[index * 10 + 5]);
+  vec2 scale = vec2(instances[index * 10 + 3], instances[index * 10 + 4]);
+  float theta = instances[index * 10 + 5];
   vec4 color = vec4(instances[index * 10 + 6], instances[index * 10 + 7], instances[index * 10 + 8], instances[index * 10 + 9]);
 
   // quad positions (-1, -1), (-1, 1), (1, -1), (1, 1), ccw in screen space.
   int vert_index = gl_VertexIndex % 4;
   vec2 position = vec2(vert_index / 2, vert_index % 2) * 2.f - 1.f;
 
-  float theta = scale_rot.z;
   mat2 rot = mat2(cos(theta), sin(theta), -sin(theta), cos(theta));
-  mat2 scale = mat2(scale_rot.x, 0.f, 0.f, scale_rot.y);
 
   float confidence_radius = 3.f;
 
-  gl_Position = vec4(ndc_position + vec3(rot * scale * position * confidence_radius, 0.f), 1.f);
+  gl_Position = vec4(ndc_position + vec3(rot * (scale * position) * confidence_radius, 0.f), 1.f);
   out_color = color;
   out_position = position * confidence_radius;
 }
