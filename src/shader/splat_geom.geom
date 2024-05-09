f142b5b70c232c99e28456f745f2d21781009348
Jaesung Park <jaesung.cs@gmail.com>
2024-05-09 20:49:49 +0900

    test geom shader

---
+++ src/shader/splat_geom.geom
@@ -0,0 +1,31 @@
+#version 460
+
+layout (points) in;
+layout (triangle_strip, max_vertices = 4) out;
+
+layout (location = 0) in vec3 in_scale_rot[];
+layout (location = 1) in vec4 in_color[];
+
+layout (location = 0) out vec4 out_color;
+layout (location = 1) out vec2 out_position;
+
+void main() {
+  vec2 scale = in_scale_rot[0].xy;
+  float theta = in_scale_rot[0].z;
+
+  // quad positions (-1, -1), (-1, 1), (1, -1), (1, 1), ccw in screen space.
+  mat4x2 positions = mat4x2(vec2(-1.f, -1.f), vec2(-1.f, 1.f), vec2(1.f, -1.f), vec2(1.f, 1.f));
+  
+  mat2 rot = mat2(cos(theta), sin(theta), -sin(theta), cos(theta));
+  mat2 scale_mat = mat2(scale.x, 0.f, 0.f, scale.y);
+  float confidence_radius = 3.f;
+  mat4x2 screen_positions = confidence_radius * rot * scale_mat * positions;
+
+  for (int i = 0; i < 4; ++i) {
+    gl_Position = gl_in[0].gl_Position + vec4(screen_positions[i], 0.f, 0.f);
+    out_color = in_color[0];
+    out_position = confidence_radius * positions[i];
+    EmitVertex();
+  }
+  EndPrimitive();
+}
--- src/vkgs/engine/engine.cc
+++ src/vkgs/engine/engine.cc
@@ -47,6 +47,8 @@
 #include "generated/inverse_index_comp.h"
 #include "generated/splat_vert.h"
 #include "generated/splat_frag.h"
+#include "generated/splat_geom_vert.h"
+#include "generated/splat_geom_geom.h"
 #include "generated/color_vert.h"
 #include "generated/color_frag.h"
 
@@ -340,6 +342,66 @@ class Engine::Impl {
         pipeline_info.render_pass = render_passes_[key];
         pipeline_info.samples = key.samples;
         splat_pipelines_[key] = vk::GraphicsPipeline(context_, pipeline_info);
+      }
+    }
+
+    // splat geom pipeline
+    {
+      std::vector<VkPipelineColorBlendAttachmentState> color_blend_attachments(
+          1);
+      color_blend_attachments[0] = {};
+      color_blend_attachments[0].blendEnable = VK_TRUE;
+      color_blend_attachments[0].srcColorBlendFactor = VK_BLEND_FACTOR_ONE;
+      color_blend_attachments[0].dstColorBlendFactor =
+          VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
+      color_blend_attachments[0].colorBlendOp = VK_BLEND_OP_ADD;
+      color_blend_attachments[0].srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
+      color_blend_attachments[0].dstAlphaBlendFactor =
+          VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
+      color_blend_attachments[0].alphaBlendOp = VK_BLEND_OP_ADD;
+      color_blend_attachments[0].colorWriteMask =
+          VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT |
+          VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+
+      std::vector<VkVertexInputBindingDescription> input_bindings(1);
+      input_bindings[0].binding = 0;
+      input_bindings[0].inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+      input_bindings[0].stride = sizeof(float) * 10;
+
+      std::vector<VkVertexInputAttributeDescription> input_attributes(3);
+      input_attributes[0].location = 0;
+      input_attributes[0].binding = 0;
+      input_attributes[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+      input_attributes[0].offset = 0;
+
+      input_attributes[1].location = 1;
+      input_attributes[1].binding = 0;
+      input_attributes[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+      input_attributes[1].offset = sizeof(float) * 3;
+
+      input_attributes[2].location = 2;
+      input_attributes[2].binding = 0;
+      input_attributes[2].format = VK_FORMAT_R32G32B32A32_SFLOAT;
+      input_attributes[2].offset = sizeof(float) * 6;
+
+      vk::GraphicsPipelineCreateInfo pipeline_info = {};
+      pipeline_info.layout = graphics_pipeline_layout_;
+      pipeline_info.vertex_shader = splat_geom_vert;
+      pipeline_info.geometry_shader = splat_geom_geom;
+      pipeline_info.fragment_shader = splat_frag;
+      pipeline_info.topology = VK_PRIMITIVE_TOPOLOGY_POINT_LIST;
+      pipeline_info.depth_test = true;
+      pipeline_info.depth_write = false;
+      pipeline_info.input_bindings = std::move(input_bindings);
+      pipeline_info.input_attributes = std::move(input_attributes);
+      pipeline_info.color_blend_attachments =
+          std::move(color_blend_attachments);
+
+      for (const auto& key : render_pass_keys) {
+        pipeline_info.render_pass = render_passes_[key];
+        pipeline_info.samples = key.samples;
+        splat_geom_pipelines_[key] =
+            vk::GraphicsPipeline(context_, pipeline_info);
       }
     }
 
@@ -1726,12 +1788,23 @@ class Engine::Impl {
 
     // draw splat
     if (loaded_point_count_ != 0) {
+      /*
       vkCmdBindPipeline(cb, VK_PIPELINE_BIND_POINT_GRAPHICS,
                         splat_pipelines_[{samples_, depth_format_}]);
 
       vkCmdBindIndexBuffer(cb, splat_index_buffer_, 0, VK_INDEX_TYPE_UINT32);
 
       vkCmdDrawIndexedIndirect(cb, splat_draw_indirect_, 0, 1, 0);
+      */
+
+      vkCmdBindPipeline(cb, VK_PIPELINE_BIND_POINT_GRAPHICS,
+                        splat_geom_pipelines_[{samples_, depth_format_}]);
+
+      std::vector<VkBuffer> vbs = {splat_storage_.instance};
+      std::vector<VkDeviceSize> vb_offsets = {0};
+      vkCmdBindVertexBuffers(cb, 0, vbs.size(), vbs.data(), vb_offsets.data());
+
+      vkCmdDrawIndirect(cb, splat_draw_indirect_, 0, 1, 0);
     }
 
     // draw ui
@@ -1814,6 +1887,7 @@ class Engine::Impl {
   std::map<RenderPassKey, vk::RenderPass> render_passes_;
   std::map<RenderPassKey, vk::GraphicsPipeline> color_line_pipelines_;
   std::map<RenderPassKey, vk::GraphicsPipeline> splat_pipelines_;
+  std::map<RenderPassKey, vk::GraphicsPipeline> splat_geom_pipelines_;
 
   vk::Attachment color_attachment_;
   vk::Attachment depth_attachment_;
